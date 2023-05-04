21ebfa0d3a85eb377460ef539ff7718f4465a20a
leoze <leoze@192.168.1.169>
2023-05-04 10:35:30 +0100

    changed initial commit to proper java file name Patient

---
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Patient.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lebaguette.ca2groupproject;
+
+/**
+ *
+ * @author leoze
+ */
+public class Patient {
+
+  
+}
