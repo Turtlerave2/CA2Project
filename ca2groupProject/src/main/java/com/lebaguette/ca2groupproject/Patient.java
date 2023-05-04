efcf8f46c6391f78588e117bf208558886a232a8
leoze <leoze@192.168.1.169>
2023-05-04 10:53:17 +0100

    Added class variables, added the equals and hashcode for when 2 patients are the same

--- ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Patient.java
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Patient.java
@@ -4,11 +4,51 @@
 
 package com.lebaguette.ca2groupproject;
 
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.Objects;
+
 /**
  *
  * @author leoze
  */
 public class Patient {
 
+  private String firstName;
+  private String lastName;
+  private LocalDate dateOfBirth;
+  private LocalDate dateJoined;
+  private LinkedList<Appointment> appointments;
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + Objects.hashCode(this.firstName);
+        hash = 89 * hash + Objects.hashCode(this.lastName);
+        hash = 89 * hash + Objects.hashCode(this.dateOfBirth);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) obj;
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        return Objects.equals(this.dateOfBirth, other.dateOfBirth);
+    }
+  
   
 }
