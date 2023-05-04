9948c7df5320c92c0507a2c6f3b865e33acb5a44
leoze <leoze@192.168.1.169>
2023-05-04 10:33:14 +0100

    Test commit

---
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Ca2groupProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lebaguette.ca2groupproject;
+
+/**
+ *
+ * @author leoze
+ */
+public class Ca2groupProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
