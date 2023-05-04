045c7ca891709a12cc641d9aae62218cce1cdbe1
leoze <leoze@192.168.1.169>
2023-05-04 11:56:09 +0100

    Added entry class inside the hashmap class, created 2 hashmap variables for now.

--- ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/hashmap.java
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/hashmap.java
@@ -4,11 +4,41 @@
  */
 package com.lebaguette.ca2groupproject;
 
+import java.util.LinkedList;
+
 /**
  * collision hash map
+ *
  * @author leoze
  */
 public class hashmap {
-    
-    
+
+    private static final int INITIAL_CAPACITY = 20;
+    private LinkedList<Entry>[] data;
+
+    private static class Entry {
+
+        private final String key;
+        private Patient value;
+
+        public Entry(String key, Patient value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public Patient getValue() {
+            return value;
+        }
+
+        public Patient updateValue(Patient newValue) {
+            Patient oldValue = value;
+            this.value = newValue;
+            return oldValue;
+        }
+    }
+
 }
