025636b3a65e1b1f2522088b0acafde74f67f21c
leoze <leoze@192.168.1.169>
2023-05-04 11:46:02 +0100

    new java class created hashmap, it will be a collision hashmap

---
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/hashmap.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lebaguette.ca2groupproject;
+
+/**
+ * collision hash map
+ * @author leoze
+ */
+public class hashmap {
+    
+    
+}
