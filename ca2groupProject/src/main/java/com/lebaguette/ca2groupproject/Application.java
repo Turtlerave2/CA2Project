ae799ad6ae635aff73a6f781f78fe37dba91433a
leoze <leoze@192.168.1.169>
2023-05-06 14:45:02 +0100

    validation to choice added, note added for each case

--- ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Application.java
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Application.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -22,74 +23,80 @@ public class Application {
         //hashmap object?
         hashmap practice = new hashmap();
         Scanner kb = new Scanner(System.in);
-        menu.menu();
 
+        int choice = -1;
         //case 6 will be the reading patient file
         //and writing to a file the data
         //and then the program will end, so 
         //the writing will be done before program closes
-        
-        int choice = kb.nextInt();
-
-        switch (choice) {
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-                   try {
-                File input = new File("patients.txt");
-                Scanner KB = new Scanner(input);
-
-                while (KB.hasNextLine()) {
-                    String line = KB.nextLine();
-                    String[] values = line.split(",");
-                    String FirstName = values[0];
-                    String LastName = values[1];
-                    LocalDate dob = LocalDate.parse(values[3]);
-                    LocalDate joinedDate = LocalDate.parse(values[4]);
-                    //need to add to like a hospital practice? gp?
-                    Patient patient = new Patient(FirstName, LastName, dob, joinedDate);
-
-                    //here
-                }
-                KB.close();
-            } catch (FileNotFoundException e) {
-                System.out.println("File was not found");
-            }
-
+        do {
+            menu.menu();
             try {
-                File input = new File("output.txt");
-                PrintWriter filewrite = new PrintWriter(input);
+                choice = kb.nextInt();
 
-                for (Patient patient
-                        : // get from the hospital practice) 
-                         {
-                    filewrite.println(patient.getFirstName() + " , " + patient.getLastName() + " , " + patient.getDateOfBirth().toString() + " , " + patient.getDateJoined().toString());
+                switch (choice) {
+                    case 1:
+                        //add patient
+                        break;
+                    case 2:
+                        //delete patient
+                        break;
+                    case 3:
+                        //display all patients
+                        break;
+                    case 4:
+                        //create new appointment for patient
+                        break;
+                    case 5:
+                        //call next patient in
+                        break;
+                    case 6:
+                   try {
+                        File input = new File("patients.txt");
+                        Scanner KB = new Scanner(input);
+
+                        while (KB.hasNextLine()) {
+                            String line = KB.nextLine();
+                            String[] values = line.split(",");
+                            String FirstName = values[0];
+                            String LastName = values[1];
+                            LocalDate dob = LocalDate.parse(values[3]);
+                            LocalDate joinedDate = LocalDate.parse(values[4]);
+                            //need to add to like a hospital practice? gp?
+                            Patient patient = new Patient(FirstName, LastName, dob, joinedDate);
+
+                            //here
+                        }
+                        KB.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("File was not found");
+                    }
+
+                    try {
+                        File input = new File("output.txt");
+                        PrintWriter filewrite = new PrintWriter(input);
+
+                        for (Patient patient
+                                : // get from the hospital practice) 
+                                 {
+                            filewrite.println(patient.getFirstName() + " , " + patient.getLastName() + " , " + patient.getDateOfBirth().toString() + " , " + patient.getDateJoined().toString());
+                        }
+                        filewrite.close();
+
+                    } catch (FileNotFoundException e) {
+                        System.out.println("File not found");
+                    }
+
+                    System.out.println("Exiting Practice Management System");
+                    return;
+                    default:
+                        System.out.println("Invalid choice, please try again.");
                 }
-                filewrite.close();
-
-            } catch (FileNotFoundException e) {
-                System.out.println("File not found");
+            } catch (InputMismatchException e) {
+                System.out.println("Enter a number for one of the options");
+                kb.next();
             }
-
-            System.out.println("Exiting Practice Management System");
-            return;
-            default:
-                System.out.println("Invalid choice, please try again.");
-        }
-
+        } while (choice != 6);
     }
 
 }
