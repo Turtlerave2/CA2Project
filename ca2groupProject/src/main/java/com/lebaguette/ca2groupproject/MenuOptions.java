47e344aff19c3e5d9006d8459b1323165d585c61
leoze <leoze@192.168.1.169>
2023-05-06 12:57:44 +0100

    New class, Menu Options, which just holds the menu options to display

---
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/MenuOptions.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lebaguette.ca2groupproject;
+
+/**
+ *
+ * @author leoze
+ */
+public class MenuOptions {
+    
+    public void menu(){
+    System.out.println("Please choose the number corresponding the option:");
+            System.out.println("1. Add a new patient");
+            System.out.println("2. Delete a patient");
+            System.out.println("3. Display all patients");
+            System.out.println("4. Create a new appointment for a patient");
+            System.out.println("5. Call the next patient in");
+            System.out.println("6. Exit");
+    }
+}
