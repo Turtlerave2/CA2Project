48c0b5ced7f246b0117310c6151ed93c6b7964bb
Rob <124254988+Robbi333@users.noreply.github.com>
2023-05-07 19:23:06 +0100

    updated Appointment class includes javadoc, created linkedlist class ```

--- ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Appointment.java
+++ ca2groupProject/src/main/java/com/lebaguette/ca2groupproject/Appointment.java
@@ -8,6 +8,15 @@ import java.util.Objects;
  * @author Rob
  */
 
+/**
+ * Represents an appointment with the specified information.
+ * Appointments are made up of the patient's details, issue, date, triage level,
+ * and doctor's full name.
+ * Two appointments are considered equal if they have the same patient data,
+ * issue, and date.
+ * The natural order for appointments is based on the triage level, where 1 has
+ * a higher priority than 5.
+ */
 public class Appointment implements Comparable<Appointment> {
     private String firstName;
     private String lastName;
@@ -17,8 +26,19 @@ public class Appointment implements Comparable<Appointment> {
     private int triageLevel;
     private String doctorFullName;
 
+    /**
+     * Constructs an Appointment object with the specified information.
+     *
+     * @param firstName      the patient's first name
+     * @param lastName       the patient's last name
+     * @param dateOfBirth    the patient's date of birth
+     * @param issue          the issue for the appointment
+     * @param date           the date of the appointment
+     * @param triageLevel    the triage level of the appointment
+     * @param doctorFullName the full name of the doctor
+     */
     public Appointment(String firstName, String lastName, LocalDate dateOfBirth, String issue, LocalDate date,
-                       int triageLevel, String doctorFullName) {
+            int triageLevel, String doctorFullName) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
@@ -28,38 +48,83 @@ public class Appointment implements Comparable<Appointment> {
         this.doctorFullName = doctorFullName;
     }
 
+    /**
+     * Returns the patient's first name.
+     *
+     * @return the patient's first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Returns the patient's last name.
+     *
+     * @return the patient's last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Returns the patient's date of birth.
+     *
+     * @return the patient's date of birth
+     */
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
+    /**
+     * Returns the issue for the appointment.
+     *
+     * @return the issue for the appointment
+     */
     public String getIssue() {
         return issue;
     }
 
+    /**
+     * Returns the date of the appointment.
+     *
+     * @return the date of the appointment
+     */
     public LocalDate getDate() {
         return date;
     }
 
+    /**
+     * Returns the triage level of the appointment.
+     *
+     * @return the triage level of the appointment
+     */
     public int getTriageLevel() {
         return triageLevel;
     }
 
+    /**
+     * Returns the full name of the doctor.
+     *
+     * @return the full name of the doctor
+     */
     public String getDoctorFullName() {
         return doctorFullName;
     }
 
+    /**
+     * Checks if this appointment is equal to another object.
+     * Two appointments are considered equal if they have the same patient data,
+     * issue, and date.
+     *
+     * @param o the object to compare with
+     * @return true if the appointments are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Appointment comp = (Appointment) o;
         return Objects.equals(firstName, comp.firstName) &&
                 Objects.equals(lastName, comp.lastName) &&
